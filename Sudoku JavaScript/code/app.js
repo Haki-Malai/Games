b0f36a00973033f9971bdf993d0f698c0a71286a
Haki-Malai <p17mala@ionio.gr>
2021-03-04 20:07:36 +0200

    Change Everything

--- Sudoku JavaScript/code/app.js
+++ Sudoku JavaScript/code/app.js
@@ -1,186 +1,82 @@
 window.addEventListener('load', ()=>{
-    let sudoku = document.querySelector('#sudoku')
-    var blocks = [], grid = []
-    let fixLeft = 0
-    for (let i = 0; i<9; i++){
-        //Init the 9x9 arrays
-        grid[i] = []
-        blocks[i] = []
-        for (let j =0; j<9; j++){
-            //Add border to bot and right side
-            if (((j==8)&&(i==8))){
-                sudoku.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block bottomBlock sideBlock\'></div>"
-            }else if (j == 8){
-                sudoku.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block bottomBlock\'></div>"
-            }else if (i== 8){
-                sudoku.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block sideBlock\'></div>"
-            }//Add element
-            else{
-                sudoku.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block\'></div>"
+    let sudoku = {
+        id : document.querySelector('#sudoku'),
+        difficulty : 'medium',
+        blocks : [],
+        inputs : [],
+        makeElements : ()=>{
+            fixLeft = 0
+            for (let i = 0; i<9; i++){
+                //Init the 9x9 arrays
+                grid[i] = []
+                sudoku.blocks[i] = []
+                sudoku.inputs[i] = []
+                for (let j =0; j<9; j++){
+                    //Add border to bot and right side
+                    if (((j==8)&&(i==8))){
+                        sudoku.id.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block bottomBlock sideBlock\'></div>"
+                    }else if (j == 8){
+                        sudoku.id.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block bottomBlock\'></div>"
+                    }else if (i== 8){
+                        sudoku.id.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block sideBlock\'></div>"
+                    }//Add element
+                    else{
+                        sudoku.id.innerHTML += "<div id = \'block[" + i + "][" + j + "]\'class=\'block\'></div>"
+                    }
+                    sudoku.blocks[i][j] = document.getElementById('block['+i+']['+j+']')
+                    grid[i][j] = 0
+                    sudoku.blocks[i][j].style.position = 'absolute'
+                    sudoku.blocks[i][j].style.left = fixLeft*4+'em'
+                    sudoku.blocks[i][j].style.top = j*4+'em'
+                    //Add input
+                    sudoku.blocks[i][j].innerHTML += "<input id = \'input["+i+']['+j+"]\' class =\"input\" value=\"\" size =\"1\" maxlength=\"1\">"
+                    sudoku.inputs[i][j] = 'input['+i+']['+j+']'
+                    //Make border fatter where it's needed
+                    if (j % 3 == 0){
+                        sudoku.blocks[i][j].style.height = '3.7em'
+                        sudoku.blocks[i][j].style.borderTopWidth = '0.3em'
+                    }else{
+                        sudoku.blocks[i][j].style.borderTopWidth = '0.15em'
+                    }
+                    if (i % 3 == 0){
+                        sudoku.blocks[i][j].style.width = '3.7em'
+                        sudoku.blocks[i][j].style.borderLeftWidth = '0.3em'
+                    }else{
+                        sudoku.blocks[i][j].style.borderLeftWidth = '0.15em'
+                    }
+                }
+                fixLeft = fixLeft + 1
             }
-            blocks[i][j] = document.getElementById('block['+i+']['+j+']')
-            grid[i][j] = 0
-            blocks[i][j].style.position = 'absolute'
-            blocks[i][j].style.left = fixLeft*4+'em'
-            blocks[i][j].style.top = j*4+'em'
-            //Add input
-            blocks[i][j].innerHTML += "<input class =\"input\" value=\"\" size =\"1\" maxlenght=\"1\">"
-            //Make border fatter where it's needed
-            if (j % 3 == 0){
-                blocks[i][j].style.height = '3.7em'
-                blocks[i][j].style.borderTopWidth = '0.3em'
-            }else{
-                blocks[i][j].style.borderTopWidth = '0.15em'
-            }
-            if (i % 3 == 0){
-                blocks[i][j].style.width = '3.7em'
-                blocks[i][j].style.borderLeftWidth = '0.3em'
-            }else{
-                blocks[i][j].style.borderLeftWidth = '0.15em'
+        },
+        showPuzzle : ()=>{
+            for (let i=0; i<9; i++){
+                for (let j=0; j<9; j++){
+                    if (hard[1][0][i][j] != 0){
+                        document.getElementById(sudoku.inputs[i][j]).setAttribute('readonly', hard[1][0][i][j])
+                        document.getElementById(sudoku.inputs[i][j]).value = hard[1][0][i][j]
+                    }
+                }
             }
         }
-        fixLeft = fixLeft + 1
+    }
+    sudoku.makeElements()
+    sudoku.showPuzzle()
+    let menu = {
+        id : document.querySelector('#menu'),
+        restart : document.querySelector('#restart'),
+        check : document.querySelector('#check'),
+        easy : document.querySelector('#easy'),
+        medium : document.querySelector('#medium'),
+        hard : document.querySelector('#hard'),
+        solution : document.querySelector('#solution'),
+        newgame : document.querySelector('#newgame'),
+        addEventListeners : ()=>{
+            easy.addEventListener('click',()=> sudoku.difficulty = 'easy')
+            medium.addEventListener('click',()=> sudoku.difficulty = 'medium')
+            hard.addEventListener('click',()=> sudoku.difficulty = 'hard')
+            restart.addEventListener('click',()=> sudoku.restart())
+            check.addEventListener('click', ()=> sudoku.check())
+            solution.addEventListener('click', ()=> sudoku.solution())
+        }
     }
 })
-function sample(arr, size) {
-    var shuffled = arr.slice(0), i = arr.length, temp, index;
-    while (i--) {
-        index = Math.floor((i + 1) * Math.random());
-        temp = shuffled[index];
-        shuffled[index] = shuffled[i];
-        shuffled[i] = temp;
-    }
-    return shuffled.slice(0, size);
-}
-function solve(grid){
-    solveHelper(grid)
-    if (checkFilled(grid)){
-        return grid
-    }else{
-        return false
-    }
-}
-function canAssign(grid, row, col, value){
-    for(let i=0; i<9; i++){
-        if ((grid[row][i] == value) || (grid[i][col] == value) || valueInBox(grid, row, col, value) || valueInHyperBox(grid, row, col, value)){
-            return false
-        }
-    }
-    return true
-}
-function solveHelper(grid){
-    for(let i=0; i<9; i++){
-        for(let j=0; j<9; j++){
-            if(grid[i][j] == 0){
-                for(let n=1; n<10; n++){
-                    if (canAssign(grid, i, j, n)){
-                        grid[i][j] = n
-                        if(solveHelper(grid)){
-                            return grid
-                            continue
-                        }else{
-                            grid[i][j] = 0
-                        }
-                    }
-                }
-                return false
-            }
-        }
-    }
-    return true
-}
-function solutions(grid){
-    solutions = 0
-    for(let i=0; i<9; i++){
-        for(let j=0; j<9; j++){
-            if(grid[i][j] == 0){
-                for(let n=1; n<10; n++){
-                    if (canAssign(grid, i, j, n)){
-                        grid[i][j] = n
-                        if(solveHelper(grid)){
-                            solutions += 1
-                            grid[i][j] = 0
-                            continue
-                        }else{
-                            grid[i][j] = 0
-                        }
-                    }
-                }
-                return false
-            }
-        }
-    }
-    return true
-}
-function valueInBox(grid, row, col, value){
-    box_row = row - (row % 3)
-    box_col = col - (col % 3)
-
-    for (let i = box_row; i<box_row+3; i++){
-        for (let j = box_col; j<box_col+3; j++){
-            if (grid[i][j] == value){
-                return true
-            }
-        }
-    }
-    return false
-}
-function valueInHyperBox(grid, row, col, value){
-    if ((0 < row < 4) && (0 < col < 4)){
-        for (let i = 1; i<4; i++){
-            for (let j = 1; j<4; j++){
-                if (grid[i][j] == value){
-                    return true
-                }
-            }
-        }
-    }else if ((4 < row < 8) && (0 < col < 4)){
-        for (let i = 5; i<8; i++){
-            for (let j = 1; j<4; j++){
-                if (grid[i][j] == value){
-                    return true
-                }
-            }
-        }
-    }else if ((0 < row < 4) && (4 < col < 8)){
-        for (let i = 1; i<4; i++){
-            for (let j = 5; j<8; j++){
-                if (grid[i][j] == value){
-                    return true
-                }
-            }
-        }
-    }else if ((4 < row < 8) && (4 < col < 8)){
-        for (let i = 5; i<8; i++){
-            for (let j = 5; j<8; j++){
-                if (grid[i][j] == value){
-                    return true
-                }
-            }
-        }
-    }
-    return false
-}
-function checkFilled(grid){
-    for(let i=0; i<9; i++){
-        for(let j=0; j<9; j++){
-            if(grid[i][j] == 0){
-                return false
-            }
-        }
-    }
-    return true
-}
-var grid = [
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0],
-]
-solve(grid)
-console.log(grid)
--- Sudoku JavaScript/code/hyperSudokuClass.js
+++ Sudoku JavaScript/code/hyperSudokuClass.js
@@ -1,7 +1,7 @@
-const clone=[...grid]
+//const clone=[...grid]
 
 function sample(arr, size){
-    var shuffled=arr.slice(0), i=arr.length, temp, index
+    let shuffled=arr.slice(0), i=arr.length, temp, index
     while (i--){
         index=Math.floor((i + 1) * Math.random())
         temp=shuffled[index]
@@ -15,19 +15,21 @@ Math.floor(Math.random() * 10)     // returns a random integer from 0 to 9
 class HyperSudoku{
     constructor(){}
     solve(grid){
-        values=HyperSudoku.create_vertex(grid)
-        graph=values[0]
-        boxes=values[1]
-        head=values[2]
+        var vertex
+        let values=this.create_vertex(grid)
+        let graph=values[0]
+        let boxes=values[1]
+        let head=values[2]
 
-        HyperSudoku.create_edge(graph, boxes)
+        this.create_edge(vertex, graph, boxes)
 
-        boo=HyperSudoku.solvev(head)
+        boo=this.solvev(head)
         if(boo){
             cur=head
             while (cur != false){
-                (i, j)=cur.xy
-                value=cur.value
+                i=cur.xy[0]
+                j=cur.xy[1]
+                let value=cur.value
                 grid[i][j]=value
                 cur=cur.next
             }
@@ -37,12 +39,12 @@ class HyperSudoku{
         }
     }
     create_vertex(grid){
-        boxes=[]
+        let boxes=[]
         for(let i=0; i < 13; i++){
             boxes.push([])
         }
 
-        graph=[]
+        let graph=[]
         for(let i=0; i < 13; i++){
             graph.push([])
             for(let j=0; j < 9; j++){
@@ -50,15 +52,15 @@ class HyperSudoku{
             }
         }
         //initiate linked list
-        head=false
-        cur=false
+        let head=false
+        let cur=false
 
         for(let i=0; i < 9; i++){
             for(let j=0; j < 9; j++){
                 //create a vertex
-                value=grid[i][j]
-                vertex=HyperSudoku.Vertex(value)
-                vertex.xy=(i, j)
+                let value=grid[i][j]
+                let vertex= new Vertex(value)
+                vertex.xy=[i, j]
                 graph[i][j]=vertex
 
                 //link the unassigned vertex
@@ -72,7 +74,7 @@ class HyperSudoku{
                     }
                 }
                 // put the vertex in one of the regular boxes
-                box_num=-1
+                let box_num=-1
                 if(0<=i<=2){
                     if(0<=j<=2){
                         box_num=0
@@ -99,7 +101,7 @@ class HyperSudoku{
                     }
                 }
                 boxes[box_num].push(vertex)
-                vertex.box_nums.add(box_num)
+                vertex.box_nums.push(box_num)
                 //put the vertex in an additional box ifneeded
                 if((1<=i<=3)&&(1<=j<=3)){
                     box_num=9
@@ -112,16 +114,38 @@ class HyperSudoku{
                 }
                 if(box_num>=9){
                     boxes[box_num].push(vertex)
-                    vertex.box_nums.add(box_num)
+                    vertex.box_nums.push(box_num)
                 }
             }
         }
         return [graph, boxes, head]
     }
+    create_edge(vertex, graph, boxes){
+        for(let i=0;i<9;i++){
+            for(let j=0;j<9;j++){
+                vertex = graph[i][j]
+                //connect in same row and column
+                for(let c=j+1;c<9;c++){
+                    vertex.connect(graph[i][c])
+                }
+                for(let r=i+1;r<9;r++){
+                    vertex.connect(graph[r][j])
+                }
+                //connect in same box
+                for(let c=0;c<vertex.box_nums.length;c++)
+                    var box = boxes[vertex.box_nums[i]]
+                    for(let v=0;v<box.length;v++){
+                        vertex.connect(box[v])
+                    }
+            }
+        }
+    }
+
+
     solvev(vertex){
-        adjacent_value=set()
+        adjacent_value=[]
         for(let i=0;i<vertex.adjacent_vertices.length;i++){
-            adjacent_value.add(vertex.adjacent_vertices[i].value)
+            adjacent_value.push(vertex.adjacent_vertices[i].value)
         }
         all_options=[1,2,3,4,5,6,7,8,9]
         options=all_options.difference(adjacent_value)
@@ -130,7 +154,7 @@ class HyperSudoku{
             value=options.pop()
             vertex.value=value
             //if got an answer, return true
-            if ((!(vertex.nextv))||(HyperSudoku.solvev(vertex.nextv))){
+            if ((!(vertex.nextv))||(this.solvev(vertex.nextv))){
                 return true
             }
             //otherwise try another solution
@@ -142,27 +166,40 @@ class HyperSudoku{
 }
 
 class Vertex{
-    constructor(this,value){
+    constructor(value){
         this.value=value
-        this.adjacent_vertices=set()
-        this.box_nums=set()
+        this.adjacent_vertices=[]
+        this.box_nums=[]
         this.nextv=false
         this.xy=false
     }
-    connect(self,vertex){
-        this.adjacent_vertices.add(vertex)
-        vertex.adjacent_vertices.add(self)
+    connect(vertex){
+        this.adjacent_vertices.push(vertex)
+        vertex.adjacent_vertices.push(this)
     }
 }
 function set(arr) {
-    var seen={}, result=[];
-    var len=arr.len;
-    for (var i=0; i < len; i++) {
-      var el=arr[i];
-      if (!seen[el]) {
-        seen[el]=true;
-        result.push(el);
-      }
+    let seen=[], result=[];
+    let len=arr.len;
+    for (let i=0; i < len; i++) {
+        let el=arr[i];
+        if (!seen[el]) {
+            seen[el]=true;
+            result.push(el);
+        }
     }
     return result;
 }
+grid = [
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+]
+sudoku = new HyperSudoku()
+sudoku.solve(grid)
