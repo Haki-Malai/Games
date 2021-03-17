7ef14ba27aedbde4267b47515c5b79473027a78c
Haki-Malai <p17mala@ionio.gr>
2021-03-17 22:00:40 +0200

    Added ASDW Keys, grid arrays and more

--- Pac-Man JavaScript/New/js/main.js
+++ Pac-Man JavaScript/New/js/main.js
@@ -1,12 +1,9 @@
-const width = window.innerWidth
-const height = window.innerHeight
-
 const config = {
-    type: Phaser.AUTO,
+    type:   Phaser.AUTO,
     parent: 'phaser-main',
-    width: height*1.8,
+    width:  height*1.8,
     height: height,
-    scene: [ loadScene, playScene ], //, menuScene
+    scene:  [ playScene ], //, menuScene
     render: {
         pixelArt: true
     },
@@ -16,7 +13,14 @@ const config = {
             debug: true
         }
     },
-};
-
-const game = new Phaser.Game(config);
+}
+const pacman    = new Item(height/14, height/14, config.width/2-height/35, config.height/2-height/30)
+const grid      = new Item(config.height*3, config.height*3, config.width/2, config.height/2)
+const gdata     = new GameData(pacman.width*4.205, 3, 0, 0, 14, 15, 'Right', 'Right')
+const loadingPs = []
+for (let i = 0; i<10; i++){
+    loadingPs.push(new Item(pacman.width*0.7, pacman.width*0.7, width/2 - pacman.width*0.7 + i*gdata.len, height/2- pacman.width*0.7))
+}
+const game      = new Phaser.Game(config)
+console.log(gdata.grid)
 
--- Pac-Man JavaScript/New/js/playScene.js
+++ Pac-Man JavaScript/New/js/playScene.js
@@ -3,28 +3,12 @@ class playScene extends Phaser.Scene
     constructor ()
     {
         super('gameScene')
-        this.pacman = {
-            width: config.width/80,
-            height: config.width/80,
-            x: config.width/2-config.width/85-1,
-            y: config.height/2-config.width/60,
-            obj: ''
-        }
-        this.grid = {
-            width: config.height*3,
-            height: config.height*3,
-            x: config.width/2,
-            y: config.height/2,
-            obj : ''
-        }
-        this.gdata = new GameData(this.pacman.width*4.205, 3, 0, 0, 14, 15, 'Right', 'Right')
     }
 
-    preload ()
-    {
+    preload (){
         this.load.image('grid', 'assets/sprites/testgrid.png')
+        this.load.image('point', 'assets/sprites/Point.png')
         this.load.spritesheet('PacMan', 'assets/sprites/PacMan.png', { frameWidth: 85, frameHeight: 91 })
-        
     }
 
     create ()
@@ -32,48 +16,48 @@ class playScene extends Phaser.Scene
         this.anims.create({
             key: 'eat',
             frames: this.anims.generateFrameNumbers('PacMan', { frames: [ 0, 1, 2, 3 ] }),
-            frameRate: 8,
+            frameRate: 10,
             repeat: -1
         })
         this.anims.create({
             key: 'idle',
             frames: this.anims.generateFrameNumbers('PacMan', { frames: [ 0 ] }),
-            frameRate: 8,
+            frameRate: 1,
             repeat: -1
         })
-        this.pacman.obj = this.add.sprite(this.pacman.x,  this.pacman.y).setDisplaySize(this.pacman.width, this.pacman.height).setDisplayOrigin(0).setScrollFactor(0)
-        this.pacman.obj.play('eat')
-
-        this.grid.obj = this.add.image(this.grid.x, this.grid.y, 'grid').setDisplaySize(this.grid.height, this.grid.width)
+        grid.obj = this.add.image(grid.x, grid.y, 'grid').setDisplaySize(grid.height, grid.width)
+        pacman.obj = this.add.sprite(pacman.x,  pacman.y, 'PacMan').setDisplaySize(pacman.width, pacman.height).setDisplayOrigin(0).setScrollFactor(0)
+        pacman.obj.play('eat')
     }
 
     update (time, delta)
     {
-        const pointer = this.input.activePointer;
-        this.cursors = this.input.keyboard.createCursorKeys()
-
         const cam = this.cameras.main
         cam.setBackgroundColor('rgba(55, 11,27, 0.5)')
-
-        console.log(this.gdata)
-        if (this.gdata.currentDirection == 'Right'){
-            cam.scrollX += this.gdata.speed
-        } else if (this.gdata.currentDirection == 'Left'){
-            cam.scrollX -= this.gdata.speed
-        } else if (this.gdata.currentDirection == 'Up'){
-            cam.scrollY += this.gdata.speed
-        } else if (this.gdata.currentDirection == 'Down'){
-            cam.scrollY -= this.gdata.speed
+        this.cursors = this.input.keyboard.createCursorKeys()
+        this.pointer = this.input.activePointer;
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+        this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        if (gdata.currentDirection == 'Right'){
+            cam.scrollX += gdata.speed
+        } else if (gdata.currentDirection == 'Left'){
+            cam.scrollX -= gdata.speed
+        } else if (gdata.currentDirection == 'Up'){
+            cam.scrollY += gdata.speed
+        } else if (gdata.currentDirection == 'Down'){
+            cam.scrollY -= gdata.speed
         }
         
-        if ((this.cursors.left.isDown) || (pointer.leftButtonDown())){
-            this.gdata.currentDirection = 'Left'
+        if ((this.cursors.left.isDown) || (this.pointer.leftButtonDown())){
+            gdata.changeDirection('Left')
         } else if (this.cursors.right.isDown){
-            this.gdata.currentDirection = 'Right'
+            gdata.changeDirection('Right')
         } else if (this.cursors.down.isDown){
-            this.gdata.currentDirection = 'Up'
+            gdata.changeDirection('Up')
         } else if (this.cursors.up.isDown){
-            this.gdata.currentDirection = 'Down'
+            gdata.changeDirection('Down')
         }
         // else if (this.cursors.right.isDown)
         // {
